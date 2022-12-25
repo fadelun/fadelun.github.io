a327aaf63bfd7b81f10b53fd14ff1f699f9f8c4b
Muhammad Fadll <fadeljourney@gmail.com>
2022-12-25 22:10:41 +0700

    menambahkan gambar dan komponent

---
+++ src/components/MyNavbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="navbar">
+    <div class="main-logo">
+      <img
+        class="img-logo"
+        src="../assets/resource/LOGO.svg"
+        alt="FADLL - logo"
+      />
+    </div>
+    <nav>
+      <ul>
+        <li><a href="">Home</a></li>
+        <li><a href="">About</a></li>
+        <li><a href="">Project</a></li>
+        <li><a href="">Contact</a></li>
+      </ul>
+    </nav>
+  </div>
+</template>
+<script>
+export default {
+  name: "MyNavbar",
+};
+</script>
+<style></style>
