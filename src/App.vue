cff6c6df229fc32235839f3c521de00d8a02d662
Muhammad Fadll <fadeljourney@gmail.com>
2023-01-08 22:46:04 +0700

    updated resource showcase

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
   <MyHeader :widthWindow="width" />
   <main>
     <MyAbout :education="education" :languages="languages" />
+    <MyProject :showcase="showcase" />
   </main>
   <footer></footer>
 </template>
@@ -11,12 +12,14 @@
 <script>
 import MyHeader from "./components/MyHeader.vue";
 import MyAbout from "./components/MyAbout.vue";
+import MyProject from "./components/MyProject.vue";
 
 export default {
   name: "App",
   components: {
     MyHeader,
     MyAbout,
+    MyProject,
   },
   data() {
     return {
@@ -58,17 +61,43 @@ export default {
           info: "Basic",
         },
       ],
+      showcase: [
+        {
+          name: "Time tracking dashboard",
+          image: "screenshot-web.png",
+          source: "https://fadelun.github.io/time-tracking-dashboard",
+          tag: ["React js", "Tailwind"],
+        },
+        {
+          name: "Rest countries API",
+          image: "screenshot-web 2.jpg",
+          source: "https://rest-countries-api-fadelun.vercel.app/",
+          tag: ["React js", "Tailwind", "Rest API"],
+        },
+        {
+          name: " Pricing component with toggle",
+          image: "screenshot-web 3.png",
+          source: "https://fadelun.github.io/Pricing-component-with-toggle ",
+          tag: ["HTML5 ", "Tailwind"],
+        },
+        {
+          name: " bikees ",
+          image: "screenshot-web 6.png",
+          source: "https://bikees-psi.vercel.app/",
+          tag: ["React js ", "Tailwind"],
+        },
+      ],
     };
   },
-  created() {
-    fetch("https://api.github.com/users/fadelun/repos")
-      .then((response) => response.json())
-      .then((data) => {
-        this.repos = data;
-      });
+  // created() {
+  //   fetch("https://api.github.com/users/fadelun/repos")
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       this.repos = data;
+  //     });
 
-    console.log(this.repos);
-  },
+  //   console.log(this.repos);
+  // },
 };
 </script>
 
